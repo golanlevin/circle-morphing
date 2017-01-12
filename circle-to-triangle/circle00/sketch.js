b8a03c3be164036202c9224b7fb6286c48d95955
Golan Levin <golan@flong.com>
2017-01-11 20:57:56 -0500

    moved 13 to 00, made new #13

---
+++ circle-to-triangle/circle00/sketch.js
@@ -0,0 +1,44 @@
+// Convert a circle into a triangle, by resampling and Zeno's interpolation
+// Golan Levin, January 2017
+
+var radius;
+var cx, cy;
+var nCirclePoints = 120;
+var circlePoints = [];
+
+//-----------------------------------------
+function setup() {
+  createCanvas(400, 400);
+  radius = width / 2 * 0.75;
+  cx = width / 2;
+  cy = height / 2;
+
+  for (var i = 0; i < nCirclePoints; i++) {
+    var t = map(i, 0, nCirclePoints, 0, TWO_PI) - HALF_PI;
+    var x = cx + radius * cos(t);
+    var y = cy + radius * sin(t);
+    circlePoints[i] = {
+      x, y
+    };
+  }
+}
+
+//-----------------------------------------
+function draw() {
+  background(255);
+  noFill();
+  stroke(0);
+  strokeWeight(3);
+
+  beginShape();
+  for (var i = 0; i < nCirclePoints; i++) {
+    if (i % (nCirclePoints / 3) !== 0) {
+      var h = (i - 1 + nCirclePoints) % nCirclePoints;
+      var j = (i + 1 + nCirclePoints) % nCirclePoints;
+      circlePoints[i].x = (circlePoints[h].x + circlePoints[i].x + circlePoints[j].x) / 3.0;
+      circlePoints[i].y = (circlePoints[h].y + circlePoints[i].y + circlePoints[j].y) / 3.0;
+    }
+    vertex(circlePoints[i].x, circlePoints[i].y);
+  }
+  endShape(CLOSE);
+}
--- circle-to-triangle/circle13/sketch.js
+++ circle-to-triangle/circle13/sketch.js
@@ -1,48 +1,75 @@
-// Convert a circle into a triangle, by xeno's interpolation
+// Convert a circle into a triangle
+// By progressively subdividing it into 
+// a 3-gon, 6-gon, 12-gon, 24-gon, etc., 
+// with smooth interpolations.
 // Golan Levin, January 2017
 
 var radius;
-var cx, cy;
-var nCirclePoints;
-var circlePoints = [];
+var trianglePoints = [];
+var nCirclePoints = 3;
+var period = 2500;
+var nPows = 5;
 
 //-----------------------------------------
 function setup() {
   createCanvas(400, 400);
-
   radius = width / 2 * 0.75;
-  cx = width / 2;
-  cy = height / 2;
-
-  nCirclePoints = 120;
-  for (var i = 0; i < nCirclePoints; i++) {
-    var t = map(i, 0, nCirclePoints, 0, TWO_PI) - HALF_PI;
-    var x = cx + radius * cos(t);
-    var y = cy + radius * sin(t);
-    circlePoints[i] = {
-      x, y
-    };
-  }
 }
 
 //-----------------------------------------
 function draw() {
   background(255);
+  push();
+  translate(width / 2, height / 2);
+
+  var t = int(millis() / period); // 0,1,2,3,4,5,6,7...
+  var tupow = 3 * floor(pow(2, 0 + (t % nPows))); // 3,6,12,24,48...
+  var utpow = 3 * floor(pow(2, 0 + (nPows - (t % nPows) - 1))); // 48,24,12,6,3...
+  print(tupow + " " + utpow);
+  var direction = (int(millis() / (period * nPows))) % 2; // 0,1,0,1...
+  var bitupow = (direction === 0) ? tupow : utpow; // 
+
+  var frac = (millis() % period) / float(period);
+  var tfrac = (direction === 0) ? frac : (1.0 - frac);
+  tfrac = pow(tfrac, 3.0);
+
   noFill();
   stroke(0);
   strokeWeight(3);
-  strokeJoin(ROUND);
+  strokeJoin(MITER);
 
   beginShape();
-  var ptsPerTriSide = nCirclePoints / 3;
-  for (var i = 0; i < nCirclePoints; i++) {
-    if (i % ptsPerTriSide !== 0) {
-      var h = (i - 1 + nCirclePoints) % nCirclePoints;
-      var j = (i + 1 + nCirclePoints) % nCirclePoints;
-      circlePoints[i].x = (1.0*circlePoints[h].x + 1.0*circlePoints[i].x + 1.0*circlePoints[j].x) / 3.0;
-      circlePoints[i].y = (1.0*circlePoints[h].y + 1.0*circlePoints[i].y + 1.0*circlePoints[j].y) / 3.0;
+  nCirclePoints = 2 * bitupow; 
+  for (var i = 0; i <= (nCirclePoints + 1); i++) { // for good shape closure
+    if (i % 2 === 0) {
+      // the corner vertices
+      var theta = map(i, 0, nCirclePoints, 0, TWO_PI)  - HALF_PI;
+      var px = radius * cos(theta);
+      var py = radius * sin(theta);
+      vertex(px, py);
+
+    } else {
+      // the halfway vertices
+      var thetaA = map(i - 1, 0, nCirclePoints, 0, TWO_PI) - HALF_PI;
+      var thetaB = map(i + 0, 0, nCirclePoints, 0, TWO_PI) - HALF_PI;
+      var thetaC = map(i + 1, 0, nCirclePoints, 0, TWO_PI) - HALF_PI;
+
+      var pxA = radius * cos(thetaA);
+      var pyA = radius * sin(thetaA);
+      var pxB = radius * cos(thetaB);
+      var pyB = radius * sin(thetaB);
+      var pxC = radius * cos(thetaC);
+      var pyC = radius * sin(thetaC);
+
+      var pxAC = (pxA + pxC) / 2; // points halfway between flanking vertices
+      var pyAC = (pyA + pyC) / 2;
+
+      var px = map(tfrac, 0, 1, pxAC, pxB);
+      var py = map(tfrac, 0, 1, pyAC, pyB);
+      vertex(px, py);
     }
-    vertex(circlePoints[i].x, circlePoints[i].y);
   }
   endShape(CLOSE);
+
+  pop();
 }
