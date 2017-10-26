2b024174438809cea142c6281e67f7d6010e4289
Golan Levin <golan@flong.com>
2017-10-25 22:18:12 -0400

    added debug view for #06

--- circle-to-triangle/circle06/sketch.js
+++ circle-to-triangle/circle06/sketch.js
@@ -1,6 +1,8 @@
 var radius;
 var cx, cy;
 var trianglePoints = [];
+var currentRadii01;
+var bShowDebug = false;
 
 //-----------------------------------------
 function setup() {
@@ -27,7 +29,7 @@ function draw() {
   strokeWeight(3);
   strokeJoin(ROUND);
 
-  var currentRadii01 = 0.5 + 0.5 * sin(millis() / 2000.0);
+  currentRadii01 = 0.5 + 0.5 * sin(millis() / 2000.0);
   var rad = currentRadii01 * radius;
 
   beginShape();
@@ -45,4 +47,31 @@ function draw() {
     }
   }
   endShape(CLOSE);
+
+  drawDebug();
+}
+
+//-----------------------------------------
+function keyPressed() {
+  bShowDebug = !bShowDebug;
+}
+
+//-----------------------------------------
+function drawDebug() {
+  if (bShowDebug) {
+    strokeWeight(1);
+    stroke(255, 0, 0, 64);
+    for (var i = 0; i < 3; i++) {
+      var px = map(currentRadii01, 0, 1, trianglePoints[i].x, cx);
+      var py = map(currentRadii01, 0, 1, trianglePoints[i].y, cy);
+      var ang1 = (i + 1) * TWO_PI / 3.0 + HALF_PI;
+      var ang2 = (i + 2) * TWO_PI / 3.0 + HALF_PI;
+      var dang = (ang2 - ang1) / 60.0;
+      for (var t = ang1; t <= ang2; t += dang) {
+        var ax = px + currentRadii01 * radius * cos(t);
+        var ay = py + currentRadii01 * radius * sin(t);
+        line(px, py, ax, ay);
+      }
+    }
+  }
 }
